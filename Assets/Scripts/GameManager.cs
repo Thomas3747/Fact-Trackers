e2acb9442a3849e28403c3a416e9765da9b88881
Thomas3747 <thomasdodoojunior@gmail.com>
2024-12-20 05:25:23 +0000

    In Game Scene added
    1. Main Game UI has been setup and the pause menu partly implemented. At the moment, no updates are running In-Game, so the pause menu hasn't been implemented. All subsections work except for the "Help" section.
    
    2. More UI updates made to the main Title scene.
    
    3. Credits section added to the options menu.
    
    4. Separate Game managers made for both scenes.
    
    5. UI for Level scene partly setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,41 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public AudioSource nextPage;
+
     public AudioSource previousPage;
+    public AudioSource click;
+    public AudioSource bgMusic;
+    public Slider musicFXSlider;
+    public Slider soundFXSlider;
+    public float soundFX;
+    public float musicFX;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        soundFX = TitleGameManager.soundFX;
+        musicFX = TitleGameManager.musicFX;
+        soundFXSlider.value = soundFX;
+        musicFXSlider.value = musicFX;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        TitleGameManager.soundFX = soundFXSlider.value;
+        TitleGameManager.musicFX = musicFXSlider.value;
+        SoundManagement();
     }
 
-    public void NextAudio()
+    // Function to play click sound upon clicking next button
+    public void Click()
     {
-        nextPage.Play();
+        click.Play();
     }
 
+    // Function to play pageflip sound upon clicking back button
     public void PreviousAudio()
     {
         previousPage.Play();
     }
 
+    //Music playing in the title menu (Title scene)
+    public void BGMusic()
+    {
+        bgMusic.Play();
+    }
+
+    //function to Quit Game to desktop
     public void QuitGame()
     {
         Application.Quit();
     }
 
+    //Function to load Level scene
     public void LoadLevel()
     {
         SceneManager.LoadScene("Level");
+    }
+
+    //Sound settings mapped to sliders in the options menu
+    public void SoundManagement()
+    {
+        bgMusic.volume = TitleGameManager.musicFX;
+        previousPage.volume = TitleGameManager.soundFX;
+        click.volume = TitleGameManager.soundFX;
+    }
+
+    //Function to reload Title Scene
+    public void LoadTitleScene()
+    {
+        SceneManager.LoadScene("Title Scene");
     }
 }
---
+++ Assets/Scripts/TitleGameManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleGameManager : MonoBehaviour
+{
+
+    public AudioSource nextPage;
+    public AudioSource previousPage;
+    public AudioSource bgMusic;
+    public Slider musicFXSlider;
+    public Slider soundFXSlider;
+    public static float soundFX = 1;
+    public static float musicFX = 1;
+    public float initialsoundFX;
+    public float initialmusicFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialmusicFX = musicFX;
+        initialsoundFX = soundFX;
+        soundFXSlider.value = initialsoundFX;
+        musicFXSlider.value = initialmusicFX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        soundFX = soundFXSlider.value;
+        musicFX = musicFXSlider.value;
+        SoundManagement();
+    }
+
+    public void NextAudio()
+    {
+        nextPage.Play();
+    }
+
+    public void PreviousAudio()
+    {
+        previousPage.Play();
+    }    
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void SoundManagement()
+    {
+        bgMusic.volume = musicFX;
+        nextPage.volume = soundFX;
+        previousPage.volume = soundFX;
+    }
+
+
+}
+
+
